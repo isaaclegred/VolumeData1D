21f3c7389c244085c07d287746af2ed30b41e0af
Isaac Legred <inl7@cornell.edu>
2019-06-11 13:21:39 -0700

    Add a version of the python script which allows use of spectre python wrappers

--- SpRender1D.py
+++ SpRender1D.py
@@ -4,7 +4,15 @@
 #Python script to render 1D SpECTRE Volume Data using Matplotlib
 
 import glob
-import h5py
+SPECTRE_LOADED = False
+try:
+    from spectre import DataStructures
+    from spectre.IO import H5 as spectre_h5
+    SPECTRE_LOADED = True
+except:
+    print("Failed loading the SpECTRE python modules. Falling back to h5py,\
+    which may have incompatibilities with the file format of the data.")
+    import h5py
 import argparse
 import sys
 import numpy as np
@@ -91,15 +99,24 @@ def parse_cmd_line():
                         and exit (optional)')
     return vars(parser.parse_args())
 
-def get_h5_files(file_prefix):
+def get_h5_and_volfiles(file_prefix):
     '''
-    Get a list of the h5 files containing the data
-    :return list of h5 files containing volume data
+    Get a list of the volfiles contained inside of the h5 files as objects
     '''
     h5_file_names = glob.glob(file_prefix + "*.h5")
-    h5_files = [h5py.File(file_name, 'r')
-                   for file_name in h5_file_names]
-    return(h5_files)
+    h5files  = []
+    volfiles = []
+    if SPECTRE_LOADED:
+        for i, file_name in enumerate(h5_file_names):
+            h5files.append(spectre_h5.H5File(file_name, 1)),
+            volfiles.append(h5files[i].get_vol('/element_data'))
+    else:
+        for i, file_name in enumerate(h5_file_names):
+            h5files.append(h5py.File(file_name, 'r')),
+            volfiles.append(h5files[i]['element_data.vol'])
+
+
+    return(h5files, volfiles)
 
 def print_var_names(args):
 
@@ -109,12 +126,18 @@ def print_var_names(args):
     :return: None
     '''
     file_prefix = args["file_prefix"]
-    h5files = get_h5_files(file_prefix)
-    volfile = h5files[0]["element_data.vol"]
-    obs_id_0  = volfile.keys()[0]
-    grid_names = volfile[Observation_Id_0].keys()
-    grid = volfile[Observation_Id_0][grid_names[0]]
-    variables = grid.keys()
+    h5files, volfiles = get_h5_and_volfiles(file_prefix)
+    volfile = volfiles[0]
+    if SPECTRE_LOADED:
+        obs_id_0 = volfile.list_observation_ids()[0]
+        grid_names = volfile.list_grids(obs_id_0)
+        variables  = volfile.list_tensor_componenets(obs_id, grid_names[0])
+
+    else:
+        obs_id_0  = volfile.keys()[0]
+        grid_names = volfile[obs_id_0].keys()
+        grid = volfile[obs_id_0][grid_names[0]]
+        variables = grid.keys()
     variables.remove("connectivity")
     variables.remove("x-coord")
     print("Variables in H5 file:\n[%s]" % ', '.join(map(str, variables)))
@@ -132,26 +155,42 @@ def get_data(args):
     '''
     file_prefix = args["file_prefix"]
     var_name = args["var"]
-    h5files = get_h5_files(file_prefix)
-    volfiles =  [h5file['element_data.vol'] for h5file in h5files]
+    h5files, volfiles  = get_h5_and_volfiles(file_prefix)
     # Get a list of times from the first vol file, sort by time value
-    ids_times = [(obs_id,volfiles[0][obs_id].attrs['observation_value']) for
-                 obs_id in volfiles[0].keys()]
-    ids_times.sort(key = lambda pair: pair[1])
+    if SPECTRE_LOADED:
+        ids_and_times = [(obs_id,(volfiles[0]).get_observation_value(obs_id)) for
+          obs_id in (volfiles[0]).list_observation_ids()]
+    else:
+        ids_and_times = [(obs_id,volfiles[0][obs_id].attrs['observation_value']) for
+         obs_id in volfiles[0].keys()]
+    ids_and_times.sort(key = lambda pair: pair[1])
     # A dictionary with the id_and_times as keys holds the data for each time
     time_data = {}
-    for id_and_time in ids_times:
+    for id_and_time in ids_and_times:
         time_data[id_and_time] = []
         for volfile in volfiles:
-            Obs_Id_File  = volfile[id_and_time[0]]
-            local_data = []
-            local_coords = []
-            for grid_name in Obs_Id_File.keys():
-                h5_grid = Obs_Id_File[grid_name]
-                local_data += list(h5_grid[var_name])
-                local_coords += list(h5_grid['InertialCoordinates_x'])
-            local_coords_and_data  = zip(local_coords, local_data)
-            time_data[id_and_time] += local_coords_and_data
+            if SPECTRE_LOADED:
+                local_data = []
+                local_coords = []
+                for grid_name in volfile.list_grids(id_and_time[0]):
+                    local_data  += list(volfile.get_tensor_component(
+                        id_and_time[0], grid_name, var_name))
+                    local_coords += list(volfile.get_tensor_component(
+                        id_and_time[0], grid_name, 'InertialCoordinates_x'))
+                    local_coords_and_data  = zip(local_coords, local_data)
+                    time_data[id_and_time] += local_coords_and_data
+
+            else:
+                # Using h5py to retrieve data
+                Obs_Id_File  = volfile[id_and_time[0]]
+                local_data = []
+                local_coords = []
+                for grid_name in Obs_Id_File.keys():
+                    h5_grid = Obs_Id_File[grid_name]
+                    local_data += list(h5_grid[var_name])
+                    local_coords += list(h5_grid['InertialCoordinates_x'])
+                    local_coords_and_data  = zip(local_coords, local_data)
+                    time_data[id_and_time] += local_coords_and_data
         # The coordinates need to be in order for plotting purposes
         time_data[id_and_time].sort(key = lambda pair: pair[0])
         time.append(id_and_time[1])
